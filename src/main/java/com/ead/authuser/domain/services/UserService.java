65aad7decad8f6093d256f23bdf40643d85853c4
Joel <joel.viana@audora.com.br>
2024-05-21 19:34:54 -0300

    feat:implementing pagination and filters with specification

--- src/main/java/com/ead/authuser/domain/services/UserService.java
+++ src/main/java/com/ead/authuser/domain/services/UserService.java
@@ -1,16 +1,20 @@
 package com.ead.authuser.domain.services;
 
-import com.ead.authuser.domain.dtos.request.UserUpdatePasswordRequestDTO;
 import com.ead.authuser.domain.dtos.request.UserRequestDTO;
 import com.ead.authuser.domain.dtos.request.UserUpdateImageRequestDTO;
+import com.ead.authuser.domain.dtos.request.UserUpdatePasswordRequestDTO;
 import com.ead.authuser.domain.dtos.request.UserUpdateRequestDTO;
 import com.ead.authuser.domain.dtos.response.UserDTO;
+import com.ead.authuser.domain.models.UserModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface UserService {
-    List<UserDTO> findAll();
+
+    Page<UserDTO> findAll(Specification<UserModel> spec, Pageable pageable);
 
     UserDTO findById(UUID userId);
 
