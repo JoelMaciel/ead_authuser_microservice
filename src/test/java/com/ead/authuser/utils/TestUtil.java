1f746ab52ebd3ffb24e3e55fb0bdb92bd684fdfb
Joel <joel.viana@audora.com.br>
2024-05-24 00:47:45 -0300

    test: implementing integration test

--- src/test/java/com/ead/authuser/utils/TestUtil.java
+++ src/test/java/com/ead/authuser/utils/TestUtil.java
@@ -15,12 +15,31 @@ public class TestUtil {
 
     public static UserModel getUserModelMock() {
         return UserModel.builder()
-                .userId(UUID.fromString("d935ef3e-b7cf-4efb-a8ca-140ac13ae479"))
+                .userId(UUID.randomUUID())
                 .username("joelmacieltest")
                 .email("joelteste@example.com")
                 .password("123456")
+                .fullName("user1joel")
+                .phoneNumber("+55 085999884477")
                 .userStatus(UserStatus.ACTIVE)
                 .userType(UserType.STUDENT)
+                .cpf("908599956565")
+                .creationDate(OffsetDateTime.now())
+                .updateDate(OffsetDateTime.now())
+                .build();
+    }
+
+    public static UserModel getUserModelMockTwo() {
+        return UserModel.builder()
+                .userId(UUID.randomUUID())
+                .username("joelmacieltestTwo")
+                .email("joeltesterwo@example.com")
+                .password("123456")
+                .fullName("user1joeltwo")
+                .phoneNumber("+55 085999884477")
+                .userStatus(UserStatus.ACTIVE)
+                .userType(UserType.STUDENT)
+                .cpf("908599956568")
                 .creationDate(OffsetDateTime.now())
                 .updateDate(OffsetDateTime.now())
                 .build();
@@ -37,11 +56,27 @@ public class TestUtil {
                 .build();
     }
 
+    public static UserRequestDTO getInvalidUserRequestDTOMock() {
+        return UserRequestDTO.builder()
+                .username("nametest")
+                .email("joelll@gmail.com")
+                .fullName("fullnametest")
+                .build();
+    }
+
     public static UserUpdateRequestDTO getUserUpdateRequestDTO() {
         return UserUpdateRequestDTO.builder()
                 .username("maciel_update")
                 .fullName("maciel_viana_updated")
                 .phoneNumber("+55 08599998855")
+                .build();
+    }
+
+    public static UserUpdateRequestDTO getInvalidUserUpdateRequestDTO() {
+        return UserUpdateRequestDTO.builder()
+                .username(null)
+                .fullName(null)
+                .phoneNumber("")
                 .build();
     }
 
@@ -52,9 +87,17 @@ public class TestUtil {
                 .build();
     }
 
+    public static UserUpdatePasswordRequestDTO getUserUpdateInvalidPasswordRequestDTO() {
+        return UserUpdatePasswordRequestDTO.builder()
+                .password("1234567891")
+                .oldPassword("1234567777")
+                .build();
+    }
+
     public static UserUpdateImageRequestDTO getUpdateImageRequestDTO() {
         return UserUpdateImageRequestDTO.builder()
                 .imageUrl("http://image_update.jpg")
                 .build();
     }
+
 }
