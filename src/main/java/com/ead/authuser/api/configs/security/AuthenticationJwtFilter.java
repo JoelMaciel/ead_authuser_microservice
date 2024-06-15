1f0ed858aa2316cf1bdec765e20ecd2e3acc9776
Joel <joel.viana@audora.com.br>
2024-06-15 09:32:21 -0300

    refactor: method getTokenHeader

--- src/main/java/com/ead/authuser/api/configs/security/AuthenticationJwtFilter.java
+++ src/main/java/com/ead/authuser/api/configs/security/AuthenticationJwtFilter.java
@@ -51,9 +51,7 @@ public class AuthenticationJwtFilter extends OncePerRequestFilter {
 
     private String getTokenHeader(HttpServletRequest request) {
         String headerAuth = request.getHeader("Authorization");
-        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
-            return headerAuth.substring(7, headerAuth.length());
-        }
-        return null;
+        return StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")
+                ? headerAuth.substring(7) : null;
     }
 }
